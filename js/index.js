57667b1511343c01cd8b0a5af6a7f816658098ad
Aya D <dzhaicholpon@gmail.com>
2025-05-04 20:27:07 -0500

    lesson-11

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+
+// const today = new Date();
+// const thisYear = today.getFullYear();
+// const footer = document.querySelector("footer");
+// const copyright = document.createElement("p");
+// copyright.innerHTML = `&copy; ${thisYear} Designed & Coded by Aya Dzhaparbekova. Let's build something great together.`;
+// footer.appendChild(copyright);
+
+const nav = document.querySelector("nav ul");
+
+const themLi = document.createElement("li");
+themLi.classList.add("theme-toggle-item");
+
+const themeToggle = document.createElement("button");
+themeToggle.id = "themeToggle";
+themeToggle.title = "Toggle Theme";
+themeToggle.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+
+themLi.appendChild(themeToggle);
+
+nav.append(themLi);
+
+nav.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+});
+
+const icon = themeToggle.querySelector("i");
+if (document.body.classList.contains("dark-theme")) {
+  icon.classList.remove("fa-moon");
+  icon.classList.add("fa-sun");
+} else {
+  icon.classList.replace("fa-sun", "fa-moon");
+}
